4b2d5df3f5ef0644f1b201829e24c16deb9adb42
alextrex147-cloud <alextrex147@gmail.com>
2026-09-18 02:11:31 -0400

    Update productos.py

--- 03_BASE_DATOS/productos.py
+++ 03_BASE_DATOS/productos.py
@@ -1,14 +1,20 @@
-from .conexion import supabase
+from conexion import supabase
 
 
 def listar_productos(busqueda=""):
-    consulta = supabase.table("productos").select("*").order(
+
+    consulta = supabase.table(
+        "productos"
+    ).select("*").order(
         "nombre",
         desc=False
     )
 
     if busqueda:
-        consulta = consulta.ilike("nombre", f"%{busqueda}%")
+        consulta = consulta.ilike(
+            "nombre",
+            f"%{busqueda}%"
+        )
 
     respuesta = consulta.execute()
 
@@ -16,7 +22,10 @@ def listar_productos(busqueda=""):
 
 
 def obtener_producto(producto_id):
-    respuesta = supabase.table("productos").select("*").eq(
+
+    respuesta = supabase.table(
+        "productos"
+    ).select("*").eq(
         "id",
         producto_id
     ).single().execute()
@@ -24,8 +33,15 @@ def obtener_producto(producto_id):
     return respuesta.data
 
 
-def crear_producto(nombre, precio, unidad):
-    respuesta = supabase.table("productos").insert({
+def crear_producto(
+    nombre,
+    precio,
+    unidad
+):
+
+    respuesta = supabase.table(
+        "productos"
+    ).insert({
         "nombre": nombre,
         "precio": float(precio),
         "unidad": unidad
@@ -34,18 +50,32 @@ def crear_producto(nombre, precio, unidad):
     return respuesta.data[0]
 
 
-def actualizar_producto(producto_id, nombre, precio, unidad):
-    respuesta = supabase.table("productos").update({
+def actualizar_producto(
+    producto_id,
+    nombre,
+    precio,
+    unidad
+):
+
+    respuesta = supabase.table(
+        "productos"
+    ).update({
         "nombre": nombre,
         "precio": float(precio),
         "unidad": unidad
-    }).eq("id", producto_id).execute()
+    }).eq(
+        "id",
+        producto_id
+    ).execute()
 
     return respuesta.data
 
 
 def eliminar_producto(producto_id):
-    return supabase.table("productos").delete().eq(
+
+    return supabase.table(
+        "productos"
+    ).delete().eq(
         "id",
         producto_id
     ).execute()
